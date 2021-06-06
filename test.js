388c79affde652ebfd8177079d8485a7f1a5d06c
benbai123 <benbai123@gmail.com>
2021-06-06 18:31:04 +0800

    加上自訂強制 unlock 方法

--- test.js
+++ test.js
@@ -6,14 +6,27 @@ const client = require('redis').createClient({ password: 'redisTest' });
 const redlock = new Redlock( [client],
     { driftFactor: 0.01, retryCount:  0, retryDelay:  200, retryJitter:  200 } );
 
-let ttl = 5000; // 要 lock 多久
+// 強制 unlock, 正常狀況下不建議使用
+const forceUnlock = (resource)=>{
+    client.get(resource, (err, value)=>{
+        if (err || !value) return;
+        redlock.unlock({resource, value});
+    });
+}
 
-// lock, 其中 resource-to-lock 代表要 lock 的資源
-redlock.lock('resource-to-lock', ttl).then((lock)=>{
-    // resource 及 value 可記錄下來,
-    // 以便當程序重啟但 lock 還未到期時
-    // 可以使用它們來做 unlock
-    // redlock.unlock({resource: 'resource-to-lock', value: '8061abfb4625324084c99ca0b1171181'});
+let ttl = 5000; // 要 lock 多久
+let resource = 'resource-to-lock'; // 要 lock 的資源
+
+// lock
+redlock.lock(resource, ttl).then((lock)=>{
+    /* resource 及 value 可記錄下來,
+     以便當程序重啟但 lock 還未到期時
+     可以使用它們來做 unlock
+     ex: redlock.unlock({resource, value});
+
+     若未記錄 value 但需解鎖亦可用上方定義的 forceUnlock 方法
+     ex: forceUnlock(resource);
+    */
     let {resource, value} = lock
     console.log({resource, value});
     console.log('first lock success');
@@ -26,7 +39,7 @@ redlock.lock('resource-to-lock', ttl).then((lock)=>{
 });
 
 // 立即再 lock 會失敗
-redlock.lock('resource-to-lock', ttl).then((lock)=>{
+redlock.lock(resource, ttl).then((lock)=>{
     console.log('second lock success');
 }).catch(function(err) {
     console.log('falied to lock second lock');
@@ -34,7 +47,7 @@ redlock.lock('resource-to-lock', ttl).then((lock)=>{
 
 // 兩秒後再 lock 應成功
 setTimeout(()=>{
-    redlock.lock('resource-to-lock', ttl).then((lock)=>{
+    redlock.lock(resource, ttl).then((lock)=>{
         console.log('third lock success');
     }).catch(function(err) {
         console.log('falied to lock third lock');

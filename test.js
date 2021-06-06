d388c0b006517d698b821467bea61669c33e6836
benbai123 <benbai123@gmail.com>
2021-06-06 14:17:13 +0800

    補充以 resource 及 value unlock 的做法

--- test.js
+++ test.js
@@ -10,6 +10,12 @@ let ttl = 5000; // 要 lock 多久
 
 // lock, 其中 resource-to-lock 代表要 lock 的資源
 redlock.lock('resource-to-lock', ttl).then((lock)=>{
+    // resource 及 value 可記錄下來,
+    // 以便當程序重啟但 lock 還未到期時
+    // 可以使用它們來做 unlock
+    // redlock.unlock({resource: 'resource-to-lock', value: '8061abfb4625324084c99ca0b1171181'});
+    let {resource, value} = lock
+    console.log({resource, value});
     console.log('first lock success');
     // 一秒後再 unlock
     setTimeout(()=>{

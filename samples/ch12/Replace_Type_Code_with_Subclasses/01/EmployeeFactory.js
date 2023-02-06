a999fca004d8e57e3159caf49ec6d15e63c9876b
Ben <benbai123@gmail.com>
2023-02-06 16:43:12 +0800

    建立工廠方法來處理建立實體，並加入 Engineer 的 case

---
+++ samples/ch12/Replace_Type_Code_with_Subclasses/01/EmployeeFactory.js
@@ -0,0 +1,10 @@
+
+/** 書上沒有寫是放在哪裡，另開一個 factory 檔來放
+ * 假設此檔案有 import/require Engineer, Employee 等 Class
+ * */
+function createEmployee(name, type) {
+  switch (type) {
+    case "engineer": return new Engineer(name, type);
+  }
+  return new Employee(name, type);
+}

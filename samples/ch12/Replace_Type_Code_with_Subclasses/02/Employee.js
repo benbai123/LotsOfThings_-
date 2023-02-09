f6078f47f5d317fe0ed66193b9f104356ba64e0d
Ben <benbai123@gmail.com>
2023-02-09 21:05:23 +0800

    對型別代碼使用 Replace Primitive with Object

--- samples/ch12/Replace_Type_Code_with_Subclasses/02/Employee.js
+++ samples/ch12/Replace_Type_Code_with_Subclasses/02/Employee.js
@@ -3,24 +3,29 @@ class Employee {
   constructor(name, type) {
     this.validateType(type);
     this._name = name;
-    this._type = type;
+    this.type = type;
   }
 
+  // 為什麼驗證不移到 EmployeeType？
   validateType(arg) {
     if (!['engineer', 'manager', 'salesman'].includes(arg))
       throw new Error(`Employee cannot be of type ${arg}`);
   }
 
+  // 為什麼不用 get type() ?
+  get typeString() {
+  	return this._type.toString();
+  }
   get type() {
   	return this._type;
   }
   // 疑問：不用 validate?
   set type(arg) {
-  	this._type = arg;
+  	this._type = new EmployeeType(arg);
   }
   // 疑問：首字大寫其它字應該不用轉小寫？
   get capitalizedType() {
-  	return this._type.charAt(0).toUpperCase() + this._type.substr(1).toLowerCase();
+  	return this.typeString.charAt(0).toUpperCase() + this.typeString.substr(1).toLowerCase();
   }
   toString() { return `${this._name} (${this.capitalizedType})`; }
 }
---
+++ samples/ch12/Replace_Type_Code_with_Subclasses/02/EmployeeType.js
@@ -0,0 +1,9 @@
+
+class EmployeeType {
+  constructor(aString) {
+    this._value = aString;
+  }
+  toString() {
+    return this._value;
+  }
+}

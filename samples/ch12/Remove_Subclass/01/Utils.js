2f7292f249413481ac37e502c3295d28e2f4027c
Ben <benbai123@gmail.com>
2023-02-13 12:04:48 +0800

    清理函式
    清理函式，先對 createPerson 使用 [Inline Variable](https://refactoring.com/catalog/inlineVariable.html) 再對 loadFromInput 使用 [Replace Loop with Pipeline](https://refactoring.com/catalog/replaceLoopWithPipeline.html)

--- samples/ch12/Remove_Subclass/01/Utils.js
+++ samples/ch12/Remove_Subclass/01/Utils.js
@@ -1,18 +1,12 @@
 
 function createPerson(aRecord) {
-	let p;
 	switch (aRecord.gender) {
-    case 'M' : p = new Male(aRecord.name); break;
-    case 'F' : p = new Female(aRecord.name); break;
-    default : p = new Person(aRecord.name);
+    case 'M' : return new Male(aRecord.name);
+    case 'F' : return new Female(aRecord.name);
+    default : return new Person(aRecord.name);
   }
-  return p;
 }
 
 function loadFromInput(data) {
-  const result = [];
-  data.forEach(aRecord => {
-    result.push(createPerson(aRecord));
-  });
-  return result;
+  return data.map(aRecord => createPerson(aRecord));
 }

0de90e5665c9b36264c74c6e101e15eb63dd7c8a
Ben <benbai123@gmail.com>
2023-02-14 21:43:42 +0800

    將子類別換成委託類別 init

---
+++ samples/ch12/Replace_Subclass_with_Delegate/01/Booking.js
@@ -0,0 +1,17 @@
+
+class Booking {
+  constructor(show, date) {
+    this._show = show;
+    this._date = date;
+  }
+
+  get hasTalkback() {
+    return this._show.hasOwnProperty('talkback') && !this.isPeakDay;
+  }
+
+  get basePrice() {
+    let result = this._show.price;
+    if (this.isPeakDay) result += Math.round(result * 0.15);
+    return result;
+  }
+}

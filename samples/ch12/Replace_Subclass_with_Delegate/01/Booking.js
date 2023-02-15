a87bbe2b162960cf5442f97b0d02c55d38cccf22
Ben <benbai123@gmail.com>
2023-02-15 20:32:13 +0800

    修改 premium booking 的工廠函式，將新的委託類別連接到 booking 物件

--- samples/ch12/Replace_Subclass_with_Delegate/01/Booking.js
+++ samples/ch12/Replace_Subclass_with_Delegate/01/Booking.js
@@ -14,4 +14,12 @@ class Booking {
     if (this.isPeakDay) result += Math.round(result * 0.15);
     return result;
   }
+
+  /** 開頭使用底線表示不是公用介面
+   * 如果目的是讓 booking 轉變成 premium booking
+   * 就可以是公用的 ( toPremiumBooking, 類似 toString() )
+   * */
+  _bePremium(extras) {
+    this._premiumDelegate = new PremiumBookingDelegate(this, extras);
+  }
 }
